a2f8bf4538f88b6f24523221a94844ae7cf07e49
Seve Barrutia <sevebarrutia@me.com>
2021-04-29 20:23:07 -0500

    added buttons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var currentUrl = "api.openweathermap.org/data/2.5/weather?q=";
+var apiKey = "&appid=feae67b17a7db2b1dc50bc7b73015b2c&units=imperial";
+var forecastUrl = "api.openweathermap.org/data/2.5/forecast?q=";
+var long = "";
+var lat = "";
+var uvUrl =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+  lat +
+  "&lon=" +
+  long +
+  apiKey;
